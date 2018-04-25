5fefc1eeebd386ef356d7b7ded616c43dc98d6ef
darachm <dchmiller@gmail.com>
2018-04-25 17:24:45 -0400

    starting to sketch it out

---
+++ scheduler.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import igraph
+
+# 
+
+# Read in 
+
+#main
+#  take cli arguments
+#  read a folder of gcal zips, unzip and read and parse
+#  build bipartite graph of people and times
+#  build as flow problem
+#  see if flow problem can eliminate two people times things
