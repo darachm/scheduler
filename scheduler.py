fd18523091d2b85b9f32583cccf9c80cb41b09da
darachm <dchmiller@gmail.com>
2018-08-13 22:58:32 -0400

    making progress. made hairball class, and ways to build the set of persons per timeslot, and how to pick good start times

--- scheduler.py
+++ scheduler.py
@@ -11,7 +11,6 @@ import iso8601
 import math
 import datetime
 from dateutil.tz import gettz
-import matplotlib.pyplot
 
 # REFACTOR AND DOCUMENT
 
@@ -24,7 +23,7 @@ find_time_range = re.compile(r"BEGIN:VEVENT.*?DTSTART(?:;TZID=(?P<start_tz>[^:]+
 
 # This takes an ical file, finds the start and end times, then cuts
 # them up into blocks of a certain minutes resolution, default 15.
-def parse_ical_to_datetimes(string,minute_resolution=30,localize_to="America/New_York"):
+def parse_ical_to_datetimes(string,minute_resolution,localize_to="America/New_York"):
     possible_datetimes = list()
     # We decode as utf8 and replace all line endings to slurp a 
     # string in for each ical
@@ -63,7 +62,8 @@ def parse_ical_to_datetimes(string,minute_resolution=30,localize_to="America/New
 
 # This takes a zipped ical and tries to open all the calendars inside
 # (should be one) and then parse and extend each one
-def read_zipped_ical(zipped_ical, localize_to="America/New_York"):
+def read_zipped_ical(zipped_ical, minute_resolution, 
+        localize_to="America/New_York"):
     with zipfile.ZipFile(zipped_ical.path) as the_zip:
         if len(the_zip.namelist()) > 1:
             print("Wait a second ... "+zipped_ical.path+
@@ -72,25 +72,30 @@ def read_zipped_ical(zipped_ical, localize_to="America/New_York"):
         for ical_name in the_zip.namelist():
             with the_zip.open(ical_name) as ical_file:
                 parsed_ical.extend(parse_ical_to_datetimes(
-                        ical_file.read(), localize_to=localize_to , 
+                        ical_file.read(), 
+                        minute_resolution=minute_resolution,
+                        localize_to=localize_to , 
                     )   )
     return(set(parsed_ical))
 
 # This reads a dir of zipped files and if it's a zip then tries to
 # handle it
-def read_dir_of_zipped_icals(path,localize_to="America/New_York"):
+def read_dir_of_zipped_icals(path,minute_resolution,
+        localize_to="America/New_York"):
     return_dict = {}
     for zipped_ical in os.scandir(path):
         if zipped_ical.name.endswith('.zip'):
             return_dict[re.sub("@.*.zip","",zipped_ical.name)
-                ] = read_zipped_ical(zipped_ical,localize_to=localize_to)
+                ] = read_zipped_ical(zipped_ical,
+                    minute_resolution=minute_resolution,
+                    localize_to=localize_to)
     return(return_dict)
 
 def read_csv_as_meetings(path):
     with open(path,"r") as f:
         meetings = list(csv.reader(f))[1:]
     return( list( map( 
-        lambda x: [x[0].strip(), set(re.split(r"\s", x[1].strip()))] , 
+        lambda x: [x[0].strip(), x[1].strip(), set(re.split(r"\s", x[2].strip()))] , 
         meetings
         ) ) )
 
@@ -112,17 +117,15 @@ def read_csv_as_meetings(path):
 class hairball():
     def __init__(self):
         self.meetings = {}
-        self.persons = {}
-        self.rooms = {}
-        self.schedule = []
-        self.minimum_window = 60
-    def append_meeting(self,meeting_id,meeting_persons,meeting_rooms,meeting_times):
-        self.meetings[meeting_id]
-        self.tl0.append(ntl0) # type of source
-        self.tl1.append(ntl1) # type of target
-        self.cl.append(ncl)   # list of capacity of that edge
-        self.weightl.append(nweightl)   # list of weights
+        self.persons_by_time = {}
+    def append_meeting(self,meeting_id,meeting_persons,meeting_times):
+        self.meetings[meeting_id] = { 'persons': meeting_persons, 'plausible_times': meeting_times }
         return(self)
+    def set_up_people(self,some_start_time,persons):
+        try:
+            self.persons_by_time[some_start_time] = self.persons_by_time[some_start_time].union(set(persons))
+        except:
+            self.persons_by_time[some_start_time] = set(persons)
 
 if __name__ == "__main__":
 
@@ -131,35 +134,68 @@ if __name__ == "__main__":
     parser.add_argument('--meetings',required=True,type=str)
     parser.add_argument('--rooms',required=True,type=str)
     parser.add_argument('--debug',default=1,type=int)
+    parser.add_argument('--resolution',default=30,type=int)
     args = parser.parse_args()
     debug = args.debug
 
-    people_datetimes = read_dir_of_zipped_icals(args.people,localize_to="America/New_York")
-    room_datetimes   = read_dir_of_zipped_icals(args.rooms)
+    people_datetimes = read_dir_of_zipped_icals(args.people,
+        minute_resolution=args.resolution,
+        localize_to="America/New_York")
+    room_datetimes   = read_dir_of_zipped_icals(args.rooms,
+        minute_resolution=args.resolution,
+        localize_to="America/New_York")
     meetings         = read_csv_as_meetings("meetings.csv")
 
     hairball = hairball()
 
     for each_meeting in meetings:
+
         this_meeting_id = each_meeting[0]
-        participants = each_meeting[1]
+        duration = each_meeting[1]
+        participants = each_meeting[2]
+
         this_meeting_possible_times = set()
+
         for each_participant in participants:
             this_meeting_possible_times = \
                 this_meeting_possible_times.union(\
                     people_datetimes[each_participant])
+
         for this_room in room_datetimes.keys():
             this_meeting_room_possibile_times = \
                 this_meeting_possible_times.union(\
                 room_datetimes[this_room])
-        hairball.meetings
 
-            for this_time in this_meeting_room_possibile_times:
-                print(this_time)
-            for each_participant in participants:
-                pass#print("Participant "+each_participant)
-                for this_time in this_meeting_room_possibile_times.union( people_datetimes[each_participant]):
-                    pass#print("Time "+str(this_time))
+        this_meeting_possible_starts = sorted(list(this_meeting_possible_times))
+        starts = list()
 
+        for i in range(len(this_meeting_possible_starts)):
+            use_it = 1
+            for j in range(1,-(-int(duration)//int(args.resolution))):
+                if  (this_meeting_possible_starts[i]+datetime.timedelta(minutes=args.resolution)*j) not in this_meeting_possible_times:
+                    use_it = 0
+                    break
+            if use_it == 1:
+                starts.append(this_meeting_possible_starts[i])
+
+
+        hairball.append_meeting(this_meeting_id, participants, starts)
+
+
+    for each_meeting_id in hairball.meetings.keys():
+        for each_time in hairball.meetings[each_meeting_id]['plausible_times']:
+            hairball.set_up_people(each_time,hairball.meetings[each_meeting_id]['persons'])
+
+
+    print(hairball.persons_by_time)
+        
+
+# next, create some sort of iterator thing that will try every combination of
+#   meeting start times
+
+# it should make a copy of the hairball persons by time before starting, and
+#   delete people out of timeslots as it goes, if it can't delete then fail
+
+# also implement rooms like the people, and do the same before persons
 
 
